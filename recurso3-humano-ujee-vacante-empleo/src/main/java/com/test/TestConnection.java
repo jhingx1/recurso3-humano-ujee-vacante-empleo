b554f3d4d9e4dd9052e787965c246f258aaaa9bc
jhingx1 <jhingx1@gmail.com>
2019-08-10 13:52:47 -0500

    Index, conexion, dao

---
+++ recurso3-humano-ujee-vacante-empleo/src/main/java/com/test/TestConnection.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.dao.DbConnection;
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		
+		DbConnection conn = new DbConnection();
+
+	}
+
+}
