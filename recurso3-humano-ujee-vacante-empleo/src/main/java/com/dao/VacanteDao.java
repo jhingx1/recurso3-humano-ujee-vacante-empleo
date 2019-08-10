f88a9319403ef2a342b9120e0d11ed41b73bd265
jhingx1 <jhingx1@gmail.com>
2019-08-10 16:43:39 -0500

    DAO para mostrar los 3 primeras vacantes.

--- recurso3-humano-ujee-vacante-empleo/src/main/java/com/dao/VacanteDao.java
+++ recurso3-humano-ujee-vacante-empleo/src/main/java/com/dao/VacanteDao.java
@@ -1,8 +1,11 @@
 package com.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.model.Vacante;
 
@@ -17,7 +20,13 @@ public class VacanteDao {
 		this.conn = conn;
 	}
 	
-	//insertar registro de vacantes
+	 /**
+     * 1. Metodo para insertar un registro en la tabla Vacante
+     *
+     * @param vacante
+     * @return Regresa el id generado por la base de datos
+     * @throws Exception
+     */
 	public boolean insert(Vacante vacante){
 		
 		String sql = "insert into Vacante values(?,?,?,?,?) ";
@@ -45,6 +54,41 @@ public class VacanteDao {
 		
 		
 	}
+	
+	/**
+     * 2. Metodo que regresa una lista con las 3 ultimas vacantes que seran
+     * mostradas en la pagina principal
+     *
+     * @return
+     * @throws Exception
+     */
+    public List<Vacante> getUltimas() {
+
+        try {
+            String sql = "select * from Vacante order by id desc limit 3";
+            PreparedStatement preparedStatement = conn.getConnection().prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            List<Vacante> list = new LinkedList<Vacante>();
+            Vacante vacante;//Objeto vacio
+            while (rs.next()) {
+            	//Creamos un objeto por cada iteracion que se almacenara en la lista
+                vacante = new Vacante(rs.getInt("id"));
+                vacante.setFechaPublicacion(rs.getDate("fechaPublicacion"));
+                vacante.setNombre(rs.getString("nombre"));
+                vacante.setDescripcion(rs.getString("descripcion"));
+                vacante.setDetalle(rs.getString("detalle"));
+                // Add vacante object to the list
+                list.add(vacante);
+            }
+            return list;
+
+        } catch (SQLException e) {            
+            System.out.println("Error VacanteDao.getUltimas: " + e.getMessage());
+            return null;
+        }
+    }
+	
+	
 	
 
 }
