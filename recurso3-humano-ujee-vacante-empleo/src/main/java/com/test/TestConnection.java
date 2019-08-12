ca0300703fcfd1f298d6186f7d33e87cda8b7e68
jhingx1 <jhingx1@gmail.com>
2019-08-12 02:20:31 -0500

    Creacion del dao usuario y haciendo la prueba de conexion usuario

--- recurso3-humano-ujee-vacante-empleo/src/main/java/com/test/TestConnection.java
+++ recurso3-humano-ujee-vacante-empleo/src/main/java/com/test/TestConnection.java
@@ -1,12 +1,17 @@
 package com.test;
 
 import com.dao.DbConnection;
+import com.dao.UsuarioDao;
+import com.model.Usuario;
 
 public class TestConnection {
 
 	public static void main(String[] args) {
 		
 		DbConnection conn = new DbConnection();
+		UsuarioDao dao = new UsuarioDao(conn);
+		Usuario usuario = dao.login("jhuaringa", "1234");
+		System.out.println(usuario);
 
 	}
 
